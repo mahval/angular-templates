0bb45e4a924b6c60dd10ceaa26fc887ed90db3b6
Peter Johan Salomonsen <pjsalomonsen@gmail.com>
2018-12-21 15:43:21 +0100

    Library setup without dependencies to avoid angular version conflicts

---
+++ ngmakelib-conf.js
@@ -0,0 +1,13 @@
+var NGMakeLib = require('ngmakelib').NGMakeLib;
+
+// Initialize NGMakeLib with entry point source file and module name
+var ngMakeLib = new NGMakeLib('src/app/lib.module.ts', 'fintechneo-angulartemplates');
+
+// Create the library and watch for changes
+if(process.argv[process.argv.length-1] === '--watch') {
+    ngMakeLib.watch();
+} else {
+    ngMakeLib.packageJSONConfig.dependencies = {};
+    ngMakeLib.packageJSONConfig.devDependencies = {};
+    ngMakeLib.build();
+}
